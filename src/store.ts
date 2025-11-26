2730bc377d92091bfafd9dfc190bc22d396d49ec
raztz15 <raztz15@gmail.com>
2025-11-26 13:31:33 +0200

    Removing unused apis from the store

--- src/store.ts
+++ src/store.ts
@@ -1,19 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReucer from './slices/UserSlice'
-import postReducer from './slices/PostsSlice'
-import { postsApi } from "./apis/Posts.api";
 import { pokemonApi } from "./apis/Pokemon.api"
 
 export const store = configureStore({
     reducer: {
-        user: userReucer,
-        post: postReducer,
-        [postsApi.reducerPath]: postsApi.reducer,
         [pokemonApi.reducerPath]: pokemonApi.reducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware()
-            .concat(postsApi.middleware)
             .concat(pokemonApi.middleware)
 })
 
